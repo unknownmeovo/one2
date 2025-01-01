3fcc836ea66221c41d3d246eebda38e290c365ac
Hadi Reza <182991763+unknownmeovo@users.noreply.github.com>
2025-01-01 18:47:47 +0500

    Add files via upload

---
+++ one2.py
@@ -0,0 +1,79 @@
+def one2ten():
+    i = 1
+    while i != 11:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2hundred():
+    i = 1
+    while i != 101:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2thousand():
+    i = 1
+    while i != 1_001:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2million():
+    i = 1
+    while i != 1_000_001:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2billion():
+    i = 1
+    while i != 1_000_000_001:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2trillion():
+    i = 1
+    while i != 1_000_000_000_001:
+        print(i)
+        i = i+1
+
+    print("x---------------------------------------------------------------------------------x")
+
+    return 0
+
+def one2about():
+    print("This module can print one to ten, one to hundred, one to thousand, one to million, one to billion, one to trillion.")
+    print("If you want to see guide use `one2usage()` function.")
+    print("This module `one2` is created by Hadi Raza and was officially completed on 1 Jan 2025.")
+    print("Email: `hadiraza.9002@gmail.com`")
+    print("     ")
+
+def one2guide():
+    print("Caution: Be careful while using it because larger numbers like million, billion, or trillion can crash, or hang your PC.")
+    print("Use `one2ten()` fuction to print 1 to 10.")
+    print("Use `one2hundred()` fuction to print 1 to 100.")
+    print("Use `one2thousnd()` fuction to print 1 to 1,000.")
+    print("Use `one2million()` fuction to print 1 to 1,000,000.")
+    print("Use `one2billion()` fuction to print 1 to 1,000,000,000.")
+    print("Use `one2trillion()` fuction to print 1 to 1,000,000,000,000.")
+    print("Use `one2about` fuction to print about thi module.")
+    print("Use `one2guide` fuction to see this guide.")
+    print("    ")
+
